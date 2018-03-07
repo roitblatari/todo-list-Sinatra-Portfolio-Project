8e5734ea0a8dc4ba4604484eac04c5d3abb0ff2d
roitblatari <roitblatari@gmail.com>
2018-03-07 12:30:38 -0500

    first commit

---
+++ db/migrate/20180307163648_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.timestamps null: false
+    end
+  end
+end
